c985947948346f2073be59deafe7ab2385723399
venkatkamraj <venkatkamraj@yahoo.com>
2017-12-28 09:41:11 -0800

    Update iOSSampleTest.java

--- src/test/java/com/appium/iPhone/iOSSampleTest.java
+++ src/test/java/com/appium/iPhone/iOSSampleTest.java
@@ -32,7 +32,7 @@ public class iOSSampleTest {
 		driver.findElement(By.className("XCUIElementTypeTextField")).clear();
 		driver.findElement(By.className("XCUIElementTypeTextField")).sendKeys("100");
 		driver.findElement(By.name("20%")).click();
-		Thread.sleep(1000);
+		Thread.sleep(5000);
 	}
 	@AfterTest
 	public void tearDown()
